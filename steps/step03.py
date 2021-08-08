e579247ce1e3fb74fd2ba9d61358a8dd1956ea4f
Shion <26x.orc.ed5.1hs@gmail.com>
2021-08-08 23:34:46 +0900

    stage 1

--- steps/step03.py
+++ steps/step03.py
@@ -22,9 +22,18 @@ class Square(Function):
         return x ** 2
 
 
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+
 if __name__ == "__main__":
-    x = Variable(np.array(10))
-    f = Square()
-    y = f(x)
-    print(type(y))
+    A = Square()
+    B = Exp()
+    C = Square()
+
+    x = Variable(np.array(0.5))
+    a = A(x)
+    b = B(a)
+    y = C(b)
     print(y.data)
--- steps/step04.py
+++ steps/step04.py
@@ -22,9 +22,27 @@ class Square(Function):
         return x ** 2
 
 
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+
+def f(x):
+    A = Square()
+    B = Exp()
+    C = Square()
+    return C(B(A(x)))
+
+
 if __name__ == "__main__":
-    x = Variable(np.array(10))
-    f = Square()
-    y = f(x)
-    print(type(y))
-    print(y.data)
+    x = Variable(np.array(0.5))
+    dy = numerical_diff(f, x)
+    print(dy)
