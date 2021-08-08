b27480fd223e60419dd93f7c0a0c76390d675525
Shion <26x.orc.ed5.1hs@gmail.com>
2021-08-08 15:17:52 +0900

    start working

---
+++ steps/step02.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Varia
