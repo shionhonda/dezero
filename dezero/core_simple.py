b284c6da2283199e37d87f51137a38acd79645a8
Shion <26x.orc.ed5.1hs@gmail.com>
2021-08-12 15:00:11 +0900

    step35

--- dezero/core_simple.py
+++ dezero/core_simple.py
@@ -198,6 +198,7 @@ def neg(x):
 
 
 def sub(x0, x1):
+    x1 = as_array(x1)
     return Sub()(x0, x1)
 
 
---
+++ dezero/functions.py
@@ -0,0 +1,41 @@
+import numpy as np
+from dezero.core import Function
+
+
+class Sin(Function):
+    def forward(self, x):
+        return np.sin(x)
+
+    def backward(self, gy):
+        (x,) = self.inputs
+        return gy * cos(x)
+
+
+def sin(x):
+    return Sin()(x)
+
+
+class Cos(Function):
+    def forward(self, x):
+        return np.cos(x)
+
+    def backward(self, gy):
+        (x,) = self.inputs
+        return -gy * sin(x)
+
+
+def cos(x):
+    return Cos()(x)
+
+
+class Tanh(Function):
+    def forward(self, x):
+        return np.tanh(x)
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        return gy * (1 - y * y)
+
+
+def tanh(x):
+    return Tanh()(x)
